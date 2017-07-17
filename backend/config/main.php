dc963ff418cb28670a8e53b76a8fbc112be2b22a
Nikolay P <prim4ick@gmail.com>
2017-07-18 00:13:26 +0300

    Remove AccessControl from backend/SiteController. Add AccessControl as global behaviour for backend app

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,14 +37,27 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<_a:(login|logout|signup)>' => 'site/<_a>',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>' => '<_c>/<_a>',
             ],
         ],
-        */
+    ],
+    'as access' => [
+        'class' => 'yii\filters\AccessControl',
+        'except' => ['site/login', 'site/error'],
+        'rules' => [
+            [
+                'allow' => true,
+                'roles' => ['@'],
+            ]
+        ],
     ],
     'params' => $params,
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<_a:(login|logout|signup)>' => 'site/<_a>',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>' => '<_c>/<_a>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
