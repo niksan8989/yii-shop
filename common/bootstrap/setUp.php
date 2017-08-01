d460fcc38361055c1af507af1a8203038943aae2
Nikolay P <prim4ick@gmail.com>
2017-08-01 23:19:12 +0300

    Добавил файл common/bootstrap/setUp.php

---
+++ common/bootstrap/setUp.php
@@ -0,0 +1,9 @@
+<?php
+namespace common\bootstrap;
+
+class setUp implements \yii\base\BootstrapInterface {
+    public function bootstrap($app)
+    {
+
+    }
+}
--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,9 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'bootstrap' => [
+        'common\bootstrap\setUp',
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\MemCache',
