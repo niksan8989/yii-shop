75ef751892eb7fe2da00b67c46c886f134b1b678
Nikolay P <prim4ick@gmail.com>
2017-10-02 16:25:45 +0300

    Добавил привязку категорий к продукту

--- shop/entities/Shop/Product.php
+++ shop/entities/Shop/Product.php
@@ -1,5 +1,6 @@
 <?php
 namespace shop\entities\Shop\Product;
+use lhs\Yii2SaveRelationsBehavior\SaveRelationsBehavior;
 use shop\entities\behaviors\MetaBehavior;
 use shop\entities\Meta;
 use shop\entities\Shop\Brand;
@@ -20,6 +21,7 @@ use yii\db\ActiveRecord;
  * @property Meta $meta
  * @property Brand $brand
  * @property Category $category
+ * @property CategoryAssignment[] $categoryAssignments
  */
 class Product extends ActiveRecord
 {
@@ -40,6 +42,37 @@ class Product extends ActiveRecord
         $this->price_new = $new;
         $this->price_old = $old;
     }
+    public function changeMainCategory($categoryId): void
+    {
+        $this->category_id = $categoryId;
+    }
+    public function assignCategory($id): void
+    {
+        $assignments = $this->categoryAssignments;
+        foreach ($assignments as $assignment) {
+            if ($assignment->isForCategory($id)) {
+                return;
+            }
+        }
+        $assignments[] = CategoryAssignment::create($id);
+        $this->categoryAssignments = $assignments;
+    }
+    public function revokeCategory($id): void
+    {
+        $assignments = $this->categoryAssignments;
+        foreach ($assignments as $i => $assignment) {
+            if ($assignment->isForCategory($id)) {
+                unset($assignments[$i]);
+                $this->categoryAssignments = $assignments;
+                return;
+            }
+        }
+        throw new \DomainException('Assignment is not found.');
+    }
+    public function revokeCategories(): void
+    {
+        $this->categoryAssignments = [];
+    }
     ##########################
     public function getBrand(): ActiveQuery
     {
@@ -48,6 +81,10 @@ class Product extends ActiveRecord
     public function getCategory(): ActiveQuery
     {
         return $this->hasOne(Category::class, ['id' => 'category_id']);
+    }
+    public function getCategoryAssignments(): ActiveQuery
+    {
+        return $this->hasMany(CategoryAssignment::class, ['product_id' => 'id']);
     }
     ##########################
     public static function tableName(): string
@@ -58,6 +95,16 @@ class Product extends ActiveRecord
     {
         return [
             MetaBehavior::className(),
+            [
+                'class' => SaveRelationsBehavior::className(),
+                'relations' => ['categoryAssignments'],
+            ],
+        ];
+    }
+    public function transactions()
+    {
+        return [
+            self::SCENARIO_DEFAULT => self::OP_ALL,
         ];
     }
 }
