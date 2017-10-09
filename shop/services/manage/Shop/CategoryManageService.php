b01dbbad977030da6dcffbb1aeea48ee1b37ca84
Nikolay P <prim4ick@gmail.com>
2017-10-09 17:31:30 +0300

    Добавил ограничение на удаление категории с товарами

--- shop/services/manage/Shop/CategoryManageService.php
+++ shop/services/manage/Shop/CategoryManageService.php
@@ -4,12 +4,16 @@ use shop\entities\Meta;
 use shop\entities\Shop\Category;
 use shop\forms\manage\Shop\CategoryForm;
 use shop\repositories\Shop\CategoryRepository;
+use shop\repositories\Shop\ProductRepository;
 class CategoryManageService
 {
     private $categories;
-    public function __construct(CategoryRepository $categories)
+    private $products;
+
+    public function __construct(CategoryRepository $categories, ProductRepository $products)
     {
         $this->categories = $categories;
+        $this->products = $products;
     }
     public function create(CategoryForm $form): Category
     {
@@ -54,6 +58,9 @@ class CategoryManageService
     {
         $category = $this->categories->get($id);
         $this->assertIsNotRoot($category);
+        if ($this->products->existsByMainCategory($category->id)) {
+            throw new \DomainException('Unable to remove category with products.');
+        }
         $this->categories->remove($category);
     }
     private function assertIsNotRoot(Category $category): void
