5cc9f38f6de617aca56ab513e7d3f0623307032c
Nikolay P <prim4ick@gmail.com>
2017-10-19 22:10:37 +0300

    Добавил транзакцию для добавления статуса продукта

---
+++ console/migrations/m171019_190846_add_shop_product_status_field.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m171019_190846_add_shop_product_status_field extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{%shop_products}}', 'status', $this->smallInteger()->notNull());
+        $this->update('{{%shop_products}}', ['status' => 1]);
+    }
+    public function down()
+    {
+        $this->dropColumn('{{%shop_products}}', 'status');
+    }
+}
